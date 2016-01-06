8656f8fdcb27e28552675b427918847de091841e
Md. Fahim Sikder <fahimsikder01@gmail.com>
2016-01-06 07:58:23 +0600

    Added Button and TextView for testing . also added Internet Permission

--- app/src/main/java/com/example/klayton/connectiontest1/MainActivity.java
+++ app/src/main/java/com/example/klayton/connectiontest1/MainActivity.java
@@ -4,13 +4,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = (Button)findViewById(R.id.button);
+        t = (TextView)findViewById(R.id.textView);
+
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                t.setText("First RUN");
+            }
+        });
+
+
     }
 
     @Override
