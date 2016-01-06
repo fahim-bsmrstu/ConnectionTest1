7eac616c529d33d045c581cebb3c725a706f5bca
Md. Fahim Sikder <fahimsikder01@gmail.com>
2016-01-06 17:45:12 +0600

    Added AsyncTask with HttpURLConnection

--- app/src/main/java/com/example/klayton/connectiontest1/MainActivity.java
+++ app/src/main/java/com/example/klayton/connectiontest1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.klayton.connectiontest1;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,6 +8,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,11 +35,82 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                t.setText("First RUN");
+                new JsonT().execute("http://jsonparsing.parseapp.com/jsonData/moviesDemoItem.txt");
+
+
             }
         });
 
 
+
+
+    }
+
+
+    public class JsonT extends AsyncTask<String, String, String>{
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+
+
+
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line = "";
+
+                while((line = reader.readLine()) != null){
+
+                    buffer.append(line);
+
+                }
+
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if(reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            t.setText(result);
+        }
     }
 
     @Override
